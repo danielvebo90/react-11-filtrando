68aa907679a82cc8e05dcf3491ac8678e134477a
danielvebo90 <danielvebo@gmail.com>
2020-02-21 22:11:51 -0500

    1 repo

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import posts from './posts'
+
+
+// Modifica el componente App para implmentar la funcionalidad requerida
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      allPosts: posts,
+      posts: posts
+    }
+  }
+  handleChange(e) {
+    let currentList = [];
+    let newList = [];
+
+    currentList = this.state.allPosts;
+    newList = currentList.filter((item, index) => item.title.toLowerCase().includes( e.target.value.toLowerCase() ));
+
+    this.setState({
+      posts: newList
+    });
+  }
+
+  render() {
+    return (
+      <div>
+       <div className="filter">
+         <input type="text" onChange={this.handleChange.bind(this)} placeholder="Ingresa el término de búsqueda" />
+       </div>
+       <ul>
+         {this.state.posts.map((post, index) => ( <li><a href={post.url}><img src={post.image } /></a><p>{ post.title }</p></li>))}
+       </ul>
+     </div>
+    )
+  }
+}
+
+
+export default App
